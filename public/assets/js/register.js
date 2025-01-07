89ac0f26999b6fb2b48ec14e641d9cb5eddfac49
Nairolf13 <fb_du_13@live.fr>
2025-01-07 13:21:23 +0100

    add view password on register and login

---
+++ public/assets/js/register.js
@@ -0,0 +1,17 @@
+function togglePasswordVisibility(inputId, toggleId) {
+    const passwordInput = document.getElementById(inputId); 
+    const toggleImage = document.getElementById(toggleId); 
+    const isPassword = passwordInput.type === 'password';  
+    
+    passwordInput.type = isPassword ? 'text' : 'password';
+
+    toggleImage.src = isPassword ? '/../../assets/imgs/oeilBarre.png' : '/../../assets/imgs/oeil.png';
+}
+
+document.getElementById('togglePassword').addEventListener('click', () => {
+    togglePasswordVisibility('password', 'togglePassword');
+});
+
+document.getElementById('toggleConfirmPassword').addEventListener('click', () => {
+    togglePasswordVisibility('confirmPassword', 'toggleConfirmPassword');
+});

35b2c33e52ba1ce8f1b2d38f4c94329e0eb30096
Nairolf13 <fb_du_13@live.fr>
2025-02-17 21:14:35 +0100

    update syntaxe welcome  page

--- public/assets/js/welcome.js
+++ public/assets/js/welcome.js
@@ -3,22 +3,27 @@ function toggleMenu() {
     navMenu.style.display = navMenu.style.display === "flex" ? "none" : "flex";
 }
 
-let currentIndex = 0;
-const itemsToShow = 1; 
-const slider = document.querySelector('.slider');
+(() => {
+    let currentIndex = 0;
+    const itemsToShow = 1;
+    const slider = document.querySelector('.slider');
 
-function moveSlider(direction) {
-    const totalItems = document.querySelectorAll('.slider li').length;
-    const maxIndex = totalItems - itemsToShow;
+    function moveSlider(direction) {
+        const totalItems = document.querySelectorAll('.slider li').length;
+        const maxIndex = totalItems - itemsToShow;
 
-    currentIndex += direction;
+        currentIndex += direction;
 
-    if (currentIndex < 0) {
-        currentIndex = 0;
-    } else if (currentIndex > maxIndex) {
-        currentIndex = maxIndex;
+        if (currentIndex < 0) {
+            currentIndex = 0;
+        } else if (currentIndex > maxIndex) {
+            currentIndex = maxIndex;
+        }
+
+        slider.style.transform = `translateX(-${currentIndex * (100 / itemsToShow)}%)`;
     }
 
-    slider.style.transform = `translateX(-${currentIndex * (100 / itemsToShow)}%)`;
-}
+    window.moveSlider = moveSlider; // Permet d'appeler moveSlider dans le HTML
+})();
+
 

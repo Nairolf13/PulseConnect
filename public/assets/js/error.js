685ad6231f35f2ee3a2740634671af7198466f28
Nairolf13 <fb_du_13@live.fr>
2025-02-18 15:42:49 +0100

    fdfdf

--- public/assets/js/error.js
+++ public/assets/js/error.js
@@ -9,7 +9,8 @@ function showErrorModal(errorMessage) {
 function showSuccessModal(successMessage) {
     document.getElementById('successMessage').textContent = successMessage;
     document.getElementById('successModal').style.display = 'block';
-
+    console.log("jjhjhj");
+    
     document.getElementById('closeSuccessModal').addEventListener('click', () => {
         document.getElementById('successModal').style.display = 'none';
         window.location.href = '/login';  
